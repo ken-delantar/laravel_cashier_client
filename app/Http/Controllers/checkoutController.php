45b33cf78b2d3d204fcbfaef64caf00e5bb65edf
ken-delantar <kenjayson83@gmail.com>
2025-02-26 02:16:46 +0800

    updated fee

--- app/Http/Controllers/checkoutController.php
+++ app/Http/Controllers/checkoutController.php
@@ -37,6 +37,12 @@ class checkoutController extends Controller
             }
 
             $price = \Stripe\Price::retrieve($request->price_id);
+            
+            $commissionRate = 0.02;
+            $totalAmount = $price->unit_amount;
+            $vendorAccount = $price->metadata->stripe_account;
+            $commissionFee = round($totalAmount * $commissionRate);
+
             $checkoutSession = \Stripe\Checkout\Session::create([
                 'line_items' => [
                     [
@@ -45,24 +51,24 @@ class checkoutController extends Controller
                     ],
                 ],
                 'mode' => 'payment',
-
                 'customer' => $customer->id,
                 'payment_method_types' => [
                     $request->payment_method,
                 ],
                 'metadata' => [
                     'order_status' => 'Pending',
-                    'vendor_id' => $price->metadata->stripe_account
+                    'vendor_id' => $vendorAccount
                 ],
                 'invoice_creation' => [
                     'enabled' => true
                 ],
                 'success_url' => $domain . '/product_listing?success=Checkout Success..',
-                'cancel_url' => $domain . '/product_listing?cancel=Checkout Successfully Failed..', 
+                'cancel_url' => $domain . '/product_listing?cancel=Checkout Successfully Failed..',
                 'payment_intent_data' => [
+                    'application_fee_amount' => $commissionFee,
                     'transfer_data' => [
-                        'destination' => $price->metadata->stripe_account
-                    ],
+                        'destination' => $vendorAccount
+                    ]
                 ],
             ]);
 
